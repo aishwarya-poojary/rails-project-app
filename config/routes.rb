6c3dcec3a7638c2bdbcc077c265cd3fd416b491e
Aishwarya <aishwarya.m@kreatio.com>
2022-08-17 17:59:53 +0530

    Add edit template and update action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # # root "articles#index"
   get 'about',to:"pages#about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
+  get 'signup',to:"users#new"
+  resources :users, expect: [:new]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
